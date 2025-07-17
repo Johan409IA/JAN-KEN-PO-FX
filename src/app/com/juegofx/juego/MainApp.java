c6b9c49849c5bcc1ac2a171447215a8d9e14df17
Johan <johan2a409@gmail.com>
2025-07-17 16:06:10 -0500

    Aplicando ultimos cambios en cuanto a configuracion y estilos. Solo falta el modo multijugador

--- src/app/com/juegofx/juego/MainApp.java
+++ src/app/com/juegofx/juego/MainApp.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
